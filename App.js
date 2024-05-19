4d8f1095587746216b6219b8c9e94f2d6c5c45f1
zaldi <akurizaldirv@gmail.com>
2024-05-19 10:36:58 +0700

    [INIT] theme color and fonts

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { DefaultTheme, PaperProvider } from "react-native-paper";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [fontsLoaded, fontError] = useFonts({
+		Poppins: require("./assets/fonts/Poppins-Regular.ttf"),
+		PoppinsMedium: require("./assets/fonts/Poppins-Medium.ttf"),
+		PoppinsBold: require("./assets/fonts/Poppins-Bold.ttf"),
+		Inter: require("./assets/fonts/Inter-Regular.otf"),
+		InterBold: require("./assets/fonts/Inter-Bold.otf"),
+		Montserrat: require("./assets/fonts/Montserrat-Regular.ttf"),
+		MontserratMedium: require("./assets/fonts/Montserrat-Medium.ttf"),
+		MontserratBold: require("./assets/fonts/Montserrat-Bold.ttf"),
+	});
+
+	if (!fontsLoaded && !fontError) {
+		return null;
+	}
+
+	const theme = {
+		...DefaultTheme,
+		colors: {
+			...DefaultTheme.colors,
+			primary: "#387ADF",
+			secondary: "#FBA834",
+			accent: "#50C4ED",
+			dark: "#2f2d2d",
+			primaryDark: "#333A73"
+		}
+	}
+
+	return (
+		<PaperProvider theme={theme}>
+			<View style={styles.container}>
+				<Text style={{ fontFamily: "MontserratBold", color: theme.colors.secondary }}>
+					Open up App.js to start working on your app!
+				</Text>
+				<StatusBar style="auto" />
+			</View>
+		</PaperProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
---
+++ assets/colors.js
@@ -0,0 +1,8 @@
+const primary = "#387ADF";
+const secondary = "#FBA834";
+const accent = "#50C4ED";
+const primaryDark = "#333A73";
+const light = "#ffffff";
+const dark = "#2f2d2d";
+
+export default { primary, secondary, accent, primaryDark, light, dark };
